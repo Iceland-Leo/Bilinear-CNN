8375070fb932b0970832e32e50d099995fdd2e05
Iceland-Leo <44286390+Iceland-Leo@users.noreply.github.com>
2019-07-23 20:31:45 +0800

    Update bilinear_resnet.py

--- bilinear_resnet.py
+++ bilinear_resnet.py
@@ -8,7 +8,7 @@ class BCNN(nn.Module):
     def __init__(self, num_classes, pretrained=True):
         super(BCNN, self).__init__()
         features = torchvision.models.resnet34(pretrained=pretrained)
-        # 去掉池化层和全连接层
+        # Remove the pooling layer and full connection layer
         self.conv = nn.Sequential(*list(features.children())[:-2])
         self.fc = nn.Linear(512 * 512, num_classes)
         self.softmax = nn.Softmax()
@@ -21,14 +21,14 @@ class BCNN(nn.Module):
 
     def forward(self, input):
         features = self.conv(input)
-        # 外积操作
+        # Cross product operation
         features = features.view(features.size(0), 512, 14 * 14)
         features_T = torch.transpose(features, 1, 2)
         features = torch.bmm(features, features_T) / (14 * 14)
         features = features.view(features.size(0), 512 * 512)
-        # 带符号的开方
+        # The signed square root
         features = torch.sign(features) * torch.sqrt(torch.abs(features) + 1e-12)
-        # l2正则化
+        # L2 regularization
         features = torch.nn.functional.normalize(features)
 
         out = self.fc(features)
